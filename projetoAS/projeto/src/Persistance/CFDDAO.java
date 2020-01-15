5c43bdfc565b56da56e37db374243cae3911c3bf
Francisco_Saraiva <francisco_saraiva94@hotmail.com>
2020-01-15 17:55:54 +0000

    CFDDAO

--- projetoAS/projeto/src/Persistance/CFDDAO.java
+++ projetoAS/projeto/src/Persistance/CFDDAO.java
@@ -7,23 +7,21 @@ import java.util.List;
 public class CFDDAO implements DAO {
 
 	public CFD get(int id) {
-		// TODO - implement CFDDAO.get //@SMELL dispensable
 		throw new UnsupportedOperationException();
 	}
 
 	public List<CFD> getAll() {
-		// TODO - implement CFDDAO.getAll //@SMELL dispensable
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void save(Object o) {
-		//@SMELL dispensable
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void update(Object o) {
-		//@SMELL dispensable
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
